ad2dd7e9ffb7f0f1b2b555b9e8d022933a705cbc
fengyu <jamesfeng2009@163.com>
2025-02-17 18:05:42 +0800

    支持h5企业微信登录

--- superset_config.example.py
+++ superset_config.example.py
@@ -3,83 +3,77 @@ from flask_appbuilder.security.manager import AUTH_DB, AUTH_OAUTH
 
 SECRET_KEY = 'lyIKAEGRDGQw5RtU7pLQgPxrSaUvBiJQW1/067h1g/UkL4N8oYYh1iiF'
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:123456@localhost:5432/superset'
-# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://superset:superset@47.93.23.80:5432/superset'
 
 # 企业微信配置参数
 WECOM_CORP_ID = 'wwc2d2bc12f207d229'
 WECOM_AGENT_ID = '1000015'
 WECOM_SECRET = 'cw97sg0T1hRcxIRNr0BuWbiVs_0O1qpQQmVEv8tE8rc'
-WECOM_REDIRECT_URI = 'http://bi.fullstack-dao.com/oauth-authorized/wecom'  # 移除端口号
+WECOM_REDIRECT_URI = 'http://bi.fullstack-dao.com/oauth-authorized/wecom'  # 使用企业微信后台配置的域名
 
-# OAuth 提供者配置（删除重复的配置）
-OAUTH_PROVIDERS = [
-    {
-        'name': 'wecom',
-        'icon': 'fa-wechat',
-        'token_key': 'access_token',
-        'remote_app': {
-            'client_id': WECOM_CORP_ID,
-            'client_secret': WECOM_SECRET,
-            'api_base_url': 'https://qyapi.weixin.qq.com/cgi-bin/',
-            'client_kwargs': {
-                'scope': 'snsapi_userinfo',
-                'verify': False
-            },
-            'request_token_url': None,
-            'access_token_method': 'GET',
-            'access_token_params': {
-                'corpid': WECOM_CORP_ID,
-                'corpsecret': WECOM_SECRET
-            },
-            'access_token_url': 'https://qyapi.weixin.qq.com/cgi-bin/gettoken',
-            'authorize_url': f'https://open.work.weixin.qq.com/wwopen/sso/qrConnect?appid={WECOM_CORP_ID}&agentid={WECOM_AGENT_ID}&redirect_uri={WECOM_REDIRECT_URI}',
-            'userinfo_url': 'https://qyapi.weixin.qq.com/cgi-bin/auth/getuserinfo'
-        }
-    }
-]
+# 使用默认的 SupersetSecurityManager
+CUSTOM_SECURITY_MANAGER = SupersetSecurityManager
 
-# 启用企业微信登录按钮
-ENABLE_WECOM_LOGIN = True
-
-# 用户注册配置
+# 认证相关配置
+AUTH_TYPE = AUTH_DB
+AUTHENTICATION_PROVIDERS = ["db", "oauth"]
 AUTH_USER_REGISTRATION = True
 AUTH_USER_REGISTRATION_ROLE = "Public"
+ENABLE_WECOM_LOGIN = True
 
-# 修改认证类型，同时支持数据库和OAuth认证
-AUTH_TYPE = AUTH_DB
+# 服务器配置
+ENABLE_PROXY_FIX = True
+WEBSERVER_ADDRESS = "0.0.0.0"
+WEBSERVER_PORT = 9000  # 改为数字类型
+PREFERRED_URL_SCHEME = 'http'
 
-# 认证提供者配置 - 同时支持数据库和OAuth认证
-AUTHENTICATION_PROVIDERS = ["db", "oauth"]
+# 代理配置
+PROXY_FIX_CONFIG = {
+    "x_for": 1,
+    "x_proto": 1,
+    "x_host": 1,
+    "x_port": 1,
+    "x_prefix": 1
+}
+
+# 登录重定向配置
+LOGIN_REDIRECT_URL = '/superset/welcome'
 
 # OAuth 提供者配置
 OAUTH_PROVIDERS = [
     {
         'name': 'wecom',
-        'icon': 'fa-wechat',
-        'token_key': 'access_token',
         'remote_app': {
             'client_id': WECOM_CORP_ID,
             'client_secret': WECOM_SECRET,
             'api_base_url': 'https://qyapi.weixin.qq.com/cgi-bin/',
             'client_kwargs': {
-                'scope': 'snsapi_userinfo',
-                'verify': False
+                'scope': 'snsapi_userinfo',  # 保持 snsapi_userinfo 用于扫码登录
+                'verify': False,
+                'token_endpoint_auth_method': 'client_secret_post'
             },
-            'request_token_url': None,
-            'access_token_method': 'GET',
-            'access_token_params': {
-                'corpid': WECOM_CORP_ID,
-                'corpsecret': WECOM_SECRET
+            'authorize_url': 'https://open.work.weixin.qq.com/wwopen/sso/qrConnect',  # 扫码登录地址
+            'h5_authorize_url': 'https://open.weixin.qq.com/connect/oauth2/authorize',  # 新增 H5 登录地址
+            'authorize_params': {
+                'appid': WECOM_CORP_ID,
+                'agentid': WECOM_AGENT_ID,
+                'redirect_uri': WECOM_REDIRECT_URI,
+                'response_type': 'code',
+                'scope': 'snsapi_userinfo'  # 保持 snsapi_userinfo
             },
-            'access_token_url': 'https://qyapi.weixin.qq.com/cgi-bin/gettoken',
-            'authorize_url': f'https://open.work.weixin.qq.com/wwopen/sso/qrConnect?appid={WECOM_CORP_ID}&agentid={WECOM_AGENT_ID}&redirect_uri={WECOM_REDIRECT_URI}',
-            'userinfo_url': 'https://qyapi.weixin.qq.com/cgi-bin/auth/getuserinfo'
+            'h5_authorize_params': {  # 新增 H5 授权参数
+                'appid': WECOM_CORP_ID,
+                'agentid': WECOM_AGENT_ID,
+                'redirect_uri': WECOM_REDIRECT_URI,
+                'response_type': 'code',
+                'scope': 'snsapi_base'  # H5 使用 snsapi_base
+            }
         }
     }
 ]
 
-
-
+# 移动端适配配置
+ENABLE_RESPONSIVE_DASHBOARD = True
+DASHBOARD_MOBILE_BREAKPOINT = 768
 
 # 其他配置
 COPILOT_URL = "http://your-copilot-url.com"

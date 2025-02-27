fc508c42acc761ab3050bc36f244326305514301
fengyu <jamesfeng2009@163.com>
2025-02-27 23:32:13 +0800

    fix 企业微信h5登录 bug

--- superset_config.example.py
+++ superset_config.example.py
@@ -75,7 +75,7 @@ OAUTH_PROVIDERS = [
             'api_base_url': 'https://qyapi.weixin.qq.com/cgi-bin/',
             'request_token_url': None,
             'access_token_url': 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={client_id}&corpsecret={client_secret}',
-            'authorize_url': f'https://open.weixin.qq.com/connect/oauth2/authorize?appid={WECOM_CORP_ID}&redirect_uri=https://bi.fullstack-dao.com/oauth-authorized/wecom_h5&response_type=code&scope=snsapi_privateinfo&state=wecom_h5#wechat_redirect',
+            'authorize_url': f'https://open.weixin.qq.com/connect/oauth2/authorize?appid={WECOM_CORP_ID}&agentid={WECOM_AGENT_ID}&redirect_uri=https://bi.fullstack-dao.com/oauth-authorized/wecom_h5&response_type=code&scope=snsapi_privateinfo&state=wecom_h5#wechat_redirect',
             'request_token_params': {
                 'scope': 'snsapi_privateinfo',
                 'response_type': 'code',
@@ -321,7 +321,7 @@ def init_oauth_views(app):
 
     def wecom_h5_login():
         """企业微信H5登录入口点"""
-        authorize_url = f'https://open.weixin.qq.com/connect/oauth2/authorize?appid={WECOM_CORP_ID}&redirect_uri=https://bi.fullstack-dao.com/oauth-authorized/wecom_h5&response_type=code&scope=snsapi_privateinfo&state=wecom_h5#wechat_redirect'
+        authorize_url = f'https://open.weixin.qq.com/connect/oauth2/authorize?appid={WECOM_CORP_ID}&agentid={WECOM_AGENT_ID}&redirect_uri=https://bi.fullstack-dao.com/oauth-authorized/wecom_h5&response_type=code&scope=snsapi_privateinfo&state=wecom_h5#wechat_redirect'
         logger.info(f"重定向到企业微信H5授权页面: {authorize_url}")
         return redirect(authorize_url)
 
